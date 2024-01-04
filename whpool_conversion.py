cef430e94a9b3adb19e3587ab87df9d7a442258c
Chibuzothon <138840210+Chibuzothon@users.noreply.github.com>
2024-01-04 14:02:33 -0500

    Started the base

--- whpool_conversion.py
+++ whpool_conversion.py
@@ -1,3 +1,15 @@
 import os
-current_woorking_dir = os.getcwd()
-print(current_woorking_dir)
+import pandas as pd
+import csv
+#import bs4 from BeautifulSoup
+from bs4 import BeautifulSoup
+
+
+root.dictionary = /Users/muzoukwu/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-0o4 
+
+
+
+
+
+
+#pd.read_html(body-517.html)
