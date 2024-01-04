4ad23a1f0bbfcf217e63a71181ea1ec4a4975483
dhabash <123644891+dhabash@users.noreply.github.com>
2024-01-04 15:16:45 -0500

    update conversion

--- whpool_conversion.py
+++ whpool_conversion.py
@@ -1,49 +1,56 @@
 import os
 import pandas as pd
+import csv 
 from csv import DictReader, DictWriter
 from pathlib import Path
 
 #should we do a 
 
+base_folder= "/Users/dhabash/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-04"
 
 
-"""root.dictionary = "/Users/muzoukwu/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-0o4"
+output_csv_path= "/Users/dhabash/Documents/GitHub/Sprinternship-2024"
 
-#notees 
-#import csv , import DicReader import DictReader 
-#from pathlib imoort Path
-#csv writer write row 
+HEADER= ["Body"]
+
+with open( output_csv_path , 'w', newline = '') as csv_file:
+        csv_writer= csv.DictWriter(csv_file, fieldnames=HEADER, extrasaction='ignore')
+
+        csv_writer.writeheader()
+
+        for folder_name, subfolders, file_names in os.walk(base_folder):
+            for file_name in file_names:
+                file_path = os.path.join(folder_name, file_name)
+            
+            # Read the contents of the file
+            with open(file_path, 'r') as input_file:
+                file_contents = input_file.read()
+            
+            # Create a dictionary with file name and contents
+            row_data = {'File Name': file_name, 'Content': file_contents}
+            
+            # Write the row to the CSV file
+            csv_writer.writerow(row_data)
 
 
 
-#open each file in directory & write to the row  # for loop or while loop to do this
 
 
+"""Open the output CSV for writing
+with open(output_path / 'whpool.csv', 'w') as csv_file:
+            csv_writer = DictWriter(csv_file, fieldnames=HEADER, extrasaction='ignore', escapechar='\\')
+            # Add the headers
+            csv_writer.writeheader()
 
 
-
-
-
+root.dictionary = "/Users/dhabash/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-0o4"
 
 all_data = []
 
 def parse_email(folder,filename)
     with open(os.path.join(folder,filename), 'r', encoding = 'utf-8') as file
-<<<<<<< HEAD
-    soup = BeautifulSoup 
-
-=======
-    soup = BeautifulSoup (fi)"""
->>>>>>> ceea49ac0b25c2efcb014a81a26aaefd6a06155b
+    soup = BeautifulSoup (fi)
 
 
-"""# Open the output CSV for writing
-        with open(output_path / 'whpool.csv', 'w') as csv_file:
-            csv_writer = DictWriter(csv_file, fieldnames=HEADER, extrasaction='ignore', escapechar='\\')
-            # Add the headers
-            csv_writer.writeheader()"""
-
-
-#pd.read_html(body-517.html)
-
+#pd.read_html(body-517.html) """
 
