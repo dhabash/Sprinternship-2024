ceea49ac0b25c2efcb014a81a26aaefd6a06155b
dhabash <123644891+dhabash@users.noreply.github.com>
2024-01-04 14:40:43 -0500

    Update whpool_conversion.py

--- whpool_conversion.py
+++ whpool_conversion.py
@@ -1,23 +1,26 @@
 import os
 import pandas as pd
-import csv
-#import bs4 from BeautifulSoup
-from bs4 import BeautifulSoup
+from csv import DictReader, DictWriter
+from pathlib import Path
 
 #should we do a 
 
 
 
-root.dictionary = "/Users/muzoukwu/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-0o4"
+"""root.dictionary = "/Users/muzoukwu/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-0o4"
 
 all_data = []
 
 def parse_email(folder,filename)
     with open(os.path.join(folder,filename), 'r', encoding = 'utf-8') as file
-    soup = BeautifulSoup (fi)
+    soup = BeautifulSoup (fi)"""
 
 
-
+"""# Open the output CSV for writing
+        with open(output_path / 'whpool.csv', 'w') as csv_file:
+            csv_writer = DictWriter(csv_file, fieldnames=HEADER, extrasaction='ignore', escapechar='\\')
+            # Add the headers
+            csv_writer.writeheader()"""
 
 
 #pd.read_html(body-517.html)
