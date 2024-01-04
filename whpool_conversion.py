3523cbfd39ee1fd11124c39339c03d6ab8d744d8
Chibuzothon <138840210+Chibuzothon@users.noreply.github.com>
2024-01-04 14:19:25 -0500

    Update whpool_conversion.py

--- whpool_conversion.py
+++ whpool_conversion.py
@@ -4,9 +4,17 @@ import csv
 #import bs4 from BeautifulSoup
 from bs4 import BeautifulSoup
 
+#should we do a 
 
-root.dictionary = /Users/muzoukwu/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-0o4 
 
+
+root.dictionary = "/Users/muzoukwu/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-0o4"
+
+all_data = []
+
+def parse_email(folder,filename)
+    with open(os.path.join(folder,filename), 'r', encoding = 'utf-8') as file
+    soup = BeautifulSoup (fi)
 
 
 
