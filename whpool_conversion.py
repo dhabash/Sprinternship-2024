656f71bdb9e6f7e3c578bb7234f2807414dc8d4f
Chibuzothon <138840210+Chibuzothon@users.noreply.github.com>
2024-01-04 15:41:01 -0500

    Working on debugging code

--- whpool_conversion.py
+++ whpool_conversion.py
@@ -6,10 +6,10 @@ from pathlib import Path
 
 #should we do a 
 
-base_folder= "/Users/dhabash/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-04"
+base_folder= "/Users/muzoukwu/Documents/GitHub/Sprinternship-2024/whpool_month_of_2021-04"
 
 
-output_csv_path= "/Users/dhabash/Documents/GitHub/Sprinternship-2024"
+output_csv_path= "/Users/muzoukwu/Documents/GitHub/Sprinternship-2024/output.csv"
 
 HEADER= ["Body"]
 
@@ -23,14 +23,14 @@ with open( output_csv_path , 'w', newline = '') as csv_file:
                 file_path = os.path.join(folder_name, file_name)
             
             # Read the contents of the file
-            with open(file_path, 'r') as input_file:
-                file_contents = input_file.read()
+                with open(file_path, 'r', encoding="ISO-8859-1"), as input_file:
+                    file_contents = input_file.read()
             
             # Create a dictionary with file name and contents
-            row_data = {'File Name': file_name, 'Content': file_contents}
+                row_data = {'File Name': file_name, 'Content': file_contents}
             
             # Write the row to the CSV file
-            csv_writer.writerow(row_data)
+                csv_writer.writerow(row_data)
 
 
 
